7e80b709e78c5eb6818d3c1d192acab7e7a4efdb
Marc Doerflinger <marc@doerf.li>
2016-05-07 00:17:29 +0200

    show created message

--- app/controllers/mocks_controller.rb
+++ app/controllers/mocks_controller.rb
@@ -2,13 +2,20 @@ class MocksController < ApplicationController
 
   def index
     @new_mock = Mock.new
+    @mocks = Mock.all
   end
 
   def create
-    @mock = Mock.new(mock_params)
-    @mock.save
+    mock = Mock.new(mock_params)
+    mock.id = SecureRandom.uuid
+    mock.save
+    flash[:new] = mock
 
-    redirect_to :action => :index
+    redirect_to mocks_path, change: ['result']
+  end
+
+  def show
+
   end
 
   private
