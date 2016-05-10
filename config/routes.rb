4eae9d99af4428c029f6580d8a795dd1e1199a3b
Marc Doerflinger <marc@doerf.li>
2016-05-10 22:52:17 +0200

    format show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  match ':id', to: 'requests#show', constraints: { id: /[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}/i}, via: [:all]
+  match ':id', to: 'requests#show', constraints: { id: /[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}/i}, via: [:all], :as => 'mocklink'
 
   resources :mocks
   resources :requests
