747cffc621f785789bd7b5b18e912b73cc3cf97e
Marc Doerflinger <marc@doerf.li>
2016-05-12 21:42:49 +0200

    inject mongoldb connection via env vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-  
+
   Mongoid.logger.level = Logger::INFO
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
