05a8a82cbec88ed0ad47e3742fda28aef9237882
Marc Doerflinger <marc@doerf.li>
2016-05-12 21:34:19 +0200

    move mongoid config

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,5 @@ module Webmock
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    Mongoid.logger.level = Logger::DEBUG
   end
 end
