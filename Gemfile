aaa8fb73c118ad49c91af87c5ab73ecfd3ef7351
Marc Doerflinger <marc@doerf.li>
2016-05-13 23:45:31 +0200

    added test

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem 'font-awesome-rails', '~> 4.6.0.0'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,8 +2,25 @@ class RequestsController < ApplicationController
 
   def show
     @mock = Mock.find( id: params[:id])
-    response.content_type = @mock.contenttype
-    render :json => @mock.body, :status => @mock.statuscode
+
+    case @mock.contetype
+      when 'application/json'
+        response.content_type = @mock.contenttype
+        render :json => @mock.body, :status => @mock.statuscode
+
+      when 'application/xml'
+        response.content_type = @mock.contenttype
+        render :xml => @mock.body, :status => @mock.statuscode
+
+      when 'text/html'
+        response.content_type = @mock.contenttype
+        render @mock.body, :status => @mock.statuscode
+
+      else
+        response.content_type = 'application/json'
+        render :json => @mock.body, :status => @mock.statuscode
+    end
+
   end
 
 end
