117bbddb7cd5de553ae398fc03349a64d856ea1a
Marc Doerflinger <marc@doerf.li>
2016-05-15 12:32:55 +0200

    fix rendering of content

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,20 +3,7 @@ class RequestsController < ApplicationController
   def show
     @mock = Mock.find( id: params[:id])
 
-    case @mock.contenttype
-      when 'application/json'
-        response.content_type = @mock.contenttype
-
-      when 'application/xml','text/xml'
-        response.content_type = @mock.contenttype
-
-      when 'text/html'
-        response.content_type = @mock.contenttype
-
-      else
-        response.content_type = 'application/json'
-    end
-
+    response.content_type = @mock.contenttype
     render plain: @mock.body, :status => @mock.statuscode
   end
 
