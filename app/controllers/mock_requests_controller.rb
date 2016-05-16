b9a09604344b52ff7e89090c999beaf3ea107144
Marc Doerflinger <marc@doerf.li>
2016-05-16 21:25:07 +0200

    save headers of request

--- app/controllers/mock_requests_controller.rb
+++ app/controllers/mock_requests_controller.rb
@@ -6,6 +6,8 @@ class MockRequestsController < ApplicationController
     MockRequest.new(
         remote_address: request.remote_ip,
         method: request.method,
+        # headers sent from client as prefixed with HTTP_ and we only want to store those and remove the HTTP_ for storing
+        headers: Hash[request.headers.env.select{|h,_| h =~ /^HTTP_(.+)/ }.map{|k,v| [k.match(/^HTTP_(.+)/)[1],v.to_s]}],
         mock: mock
     ).save
 
