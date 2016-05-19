126e0e3ad43a35abea6a7cd048163e17be1941af
Marc Doerflinger <marc@doerf.li>
2016-05-19 21:50:35 +0200

    reorder elements in timeline

--- app/assets/javascripts/components/mock_request_timeline_item.js.jsx
+++ app/assets/javascripts/components/mock_request_timeline_item.js.jsx
@@ -3,19 +3,19 @@ var MRTimelineItem = React.createClass({
         return (
             <div className="request" onClick={ (e) => this.props.onClick(e,this.props.idx)}>
                 <div className="row">
-                    <div className="col-sm-6">
+                    <div className="col-sm-4">
                         { this.props.request.method }
                     </div>
-                    <div className="col-sm-6">
-                        { this.props.request.created_at_time }
+                    <div className="col-sm-8 right">
+                        { this.props.request.remote_address }
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-sm-6">
-                        { this.props.request.remote_address }
-                    </div>
-                    <div className="col-sm-6">
+                    <div className="col-sm-5">
                         { this.props.request.created_at_date }
+                    </div>
+                    <div className="col-sm-7 right">
+                        @{ this.props.request.created_at_time }
                     </div>
                 </div>
             </div>
