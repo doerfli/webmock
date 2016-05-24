bc413ff30812188bd9d8bdc042300d77bf25c490
Marc Doerflinger <marc@doerf.li>
2016-05-24 22:23:34 +0200

    slimmer design of timeline

--- app/assets/javascripts/components/mock_request_timeline_item.js.jsx
+++ app/assets/javascripts/components/mock_request_timeline_item.js.jsx
@@ -2,22 +2,7 @@ var MRTimelineItem = React.createClass({
     render: function() {
         return (
             <div className="request" onClick={ (e) => this.props.onClick(e,this.props.idx)}>
-                <div className="row">
-                    <div className="col-sm-4">
-                        { this.props.request.method }
-                    </div>
-                    <div className="col-sm-8 right">
-                        { this.props.request.remote_address }
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm-5">
-                        { this.props.request.created_at_date }
-                    </div>
-                    <div className="col-sm-7 right">
-                        @{ this.props.request.created_at_time }
-                    </div>
-                </div>
+                <i className="fa fa-circle-o"/> { this.props.request.created_at_time } | { this.props.request.method }
             </div>
         )
     }
