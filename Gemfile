79bdfd6c11022be6d5393cbb8c95641005ade78e
Marc Doerflinger <marc@doerf.li>
2016-06-09 21:45:17 +0200

    use database cleaner

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
   gem 'rails-controller-testing'
+  gem 'database_cleaner'
 end
 
 group :development do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,18 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'database_cleaner'
+DatabaseCleaner.strategy = :truncation
+
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
+
+  setup do
+    DatabaseCleaner.start
+  end
+
+  teardown do
+    DatabaseCleaner.clean
+  end
+
 end
