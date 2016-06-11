083f214728ffd92c6f8c03292158b39bcdfe8992
Marc Doerflinger <marc@doerf.li>
2016-06-11 13:16:57 +0200

    added test

--- test/integration/mock_request_flows_test.rb
+++ test/integration/mock_request_flows_test.rb
@@ -45,4 +45,43 @@ class MockRequestFlowsTest < ActionDispatch::IntegrationTest
     assert_equal '12345678', response.headers["X-API-KEY"]
   end
 
+  test 'mock request stores incoming get request' do
+    mock = create(:mock, body: '{ "hello": "world" }', customheaders: [{ 'name': 'X-API-KEY', 'value':'12345678'}] )
+
+    get "/#{mock.id}?a=b"
+    assert_response :success
+    assert_equal '12345678', response.headers["X-API-KEY"]
+
+    m = Mock.find(mock.id)
+    assert_equal 1, m.mock_requests.size
+    r1 = m.mock_requests.first
+    assert_equal '127.0.0.1', r1.remote_address
+    assert_equal 'GET', r1.method
+    assert_equal 'application/x-www-form-urlencoded', r1.contenttype
+    assert_equal '', r1.body
+    assert_equal 0, r1.body_size
+    assert_equal "http://www.example.com/#{mock.id}?a=b", r1.url
+    assert_equal 'b', r1.query_params['a']
+  end
+
+  test 'mock request stores incoming post request' do
+    mock = create(:mock, body: '{ "hello": "world" }', customheaders: [{ 'name': 'X-API-KEY', 'value':'12345678'}] )
+
+    post "/#{mock.id}", params: {'hello': 'world'}.to_json, headers: {'CONTENT_TYPE' => 'application/json'}
+
+    assert_response :success
+    assert_equal '12345678', response.headers['X-API-KEY']
+
+    m = Mock.find(mock.id)
+    assert_equal 1, m.mock_requests.size
+    r1 = m.mock_requests.first
+    assert_equal '127.0.0.1', r1.remote_address
+    assert_equal 'POST', r1.method
+    assert_equal 'application/json', r1.contenttype
+    assert_equal '{"hello":"world"}', r1.body
+    assert_equal 17, r1.body_size
+    assert_equal "http://www.example.com/#{mock.id}", r1.url
+    assert_nil r1.query_params
+  end
+
 end
