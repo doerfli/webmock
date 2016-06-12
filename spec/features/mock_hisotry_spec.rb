1607372a1222fe88c21690fd3de0e94b31ff99ec
Marc Doerflinger <marc@doerf.li>
2016-06-12 13:20:35 +0200

    add testcase

--- spec/features/mock_hisotry_spec.rb
+++ spec/features/mock_hisotry_spec.rb
@@ -13,4 +13,19 @@ RSpec.feature 'mock history', :type => :feature, :js => true do
 
     expect(page).to have_content 'No requests received yet'
   end
+
+  scenario 'User creates a new mock, sends 3 requests and history shows those three requests' do
+    visit '/'
+
+    fill_in 'mock_body', with: "{'hello':'world'}"
+    click_button 'Create mock now!'
+
+    expect(page).to have_content 'Mock created successfully'
+
+    3.times{ click_link 'mock_link' }
+
+    click_link 'history'
+
+    expect(page).to have_css('.history .timeline .request', text: 'GET', count: 3)
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -99,5 +99,10 @@ RSpec.configure do |config|
 end
 
 require 'capybara/poltergeist'
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, { :js_errors => false })
+end
+
 #Capybara.default_driver = :selenium
 Capybara.javascript_driver = :poltergeist
