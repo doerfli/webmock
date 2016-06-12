5ef4a4f8febe677b4e53df577921ceb192dd89e1
Marc Doerflinger <marc@doerf.li>
2016-06-12 13:32:00 +0200

    add testcase

--- spec/features/mock_hisotry_spec.rb
+++ spec/features/mock_hisotry_spec.rb
@@ -14,6 +14,24 @@ RSpec.feature 'mock history', :type => :feature, :js => true do
     expect(page).to have_content 'No requests received yet'
   end
 
+  scenario 'User creates a new mock, sends 1 request and opens history' do
+    visit '/'
+
+    fill_in 'mock_body', with: "{'hello':'world'}"
+    click_button 'Create mock now!'
+
+    expect(page).to have_content 'Mock created successfully'
+
+    click_link 'mock_link'
+    click_link 'history'
+
+    expect(page).to have_css('.history .content .general', text: 'GET')
+    expect(page).to have_css('.history .content .general', text: 'http://127.0.0.1:')
+    expect(page).to have_css('.history .content .general', text: '127.0.0.1')
+    expect(page).to have_css('.history .content .headers', text: 'PhantomJS')
+    expect(page).to have_css('.history .content .body', text: 'Request contained no body data')
+  end
+
   scenario 'User creates a new mock, sends 3 requests and history shows those three requests' do
     visit '/'
 
