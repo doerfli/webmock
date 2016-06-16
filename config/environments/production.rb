653c53461b42961edf1b449fdbc2792fc3fa27d9
Marc Doerflinger <marc@doerf.li>
2016-06-16 20:42:06 +0200

    change config for actioncables production (#11)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_cable.allowed_request_origins = %w(http://webmock.io http://*.webmock.io http://localhost:3000)
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
