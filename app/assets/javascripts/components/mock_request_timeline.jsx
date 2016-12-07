13f8025d2118f49672b181ae7c3f213e3dd23d06
Marc Doerflinger <marc@doerf.li>
2016-12-07 21:50:17 +0100

    convert to es6

--- app/assets/javascripts/components/mock_request_timeline.jsx
+++ app/assets/javascripts/components/mock_request_timeline.jsx
@@ -1,13 +1,12 @@
-var MRTimeline = React.createClass({
-    render: function() {
-      return (
-        <div className="timeline">
-            {this.props.data.map(function(request,i) {
-                var isCurr = this.props.selectedId == request._id.$oid;
-                return <MRTimelineItem key={request._id.$oid} request={request} idx={i} isSelected={isCurr} onClick={this.props.onTimelineItemClick}/>
-            }.bind(this))}
-        </div>
-      )
+class MRTimeline extends React.Component {
+    render() {
+        return (
+            <div className="timeline">
+                {this.props.data.map(function(request,i) {
+                    let isCurr = this.props.selectedId == request._id.$oid;
+                    return <MRTimelineItem key={request._id.$oid} request={request} idx={i} isSelected={isCurr} onClick={this.props.onTimelineItemClick}/>
+                }.bind(this))}
+            </div>
+        )
     }
-});
-
+}
