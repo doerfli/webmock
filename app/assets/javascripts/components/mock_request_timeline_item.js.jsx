a8524a344e9b509614f405586bf958df473d4b50
Marc Doerflinger <marc@doerf.li>
2016-12-07 21:47:55 +0100

    convert to es6

--- app/assets/javascripts/components/mock_request_timeline_item.js.jsx
+++ app/assets/javascripts/components/mock_request_timeline_item.js.jsx
@@ -1,14 +1,15 @@
-var MRTimelineItem = React.createClass({
-    render: function() {
+class MRTimelineItem extends React.Component {
+    render() {
         var clsn = "request ";
         clsn += this.props.request.method;
-        if ( this.props.isSelected == true ) {
+        if (this.props.isSelected == true) {
             clsn += " selected";
         }
         return (
-            <div className={clsn} onClick={ (e) => this.props.onClick(e,this.props.idx)}>
-                <i className="fa fa-angle-right"></i> { this.props.request.created_at_time } | { this.props.request.method }
+            <div className={clsn} onClick={ (e) => this.props.onClick(e, this.props.idx)}>
+                <i className="fa fa-angle-right"/> { this.props.request.created_at_time }
+                | { this.props.request.method }
             </div>
-        )
+        );
     }
-});
+}
